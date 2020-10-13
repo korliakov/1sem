8b18fe1a60b4a2d0ca02189ef9edfd8432d33212
korliakov <korliakov.dk@phystech.edu>
2020-10-13 03:21:33 +0300

    еще чет сделяль

--- lab4/finished2.py
+++ lab4/finished2.py
@@ -23,7 +23,7 @@ def new_ball():
     global Balls
     x = randint(100, 1100)
     y = randint(100, 900)
-    r = randint(70, 100)
+    r = randint(40, 100)
     vx = randint(-5, 5)
     vy = randint(-5, 5)
     color = COLORS[randint(0, 5)]
@@ -37,22 +37,24 @@ def new_square():
     global Squares
     x = randint(100, 1100)
     y = randint(100, 900)
-    a = randint(70, 100)
-    vx = randint(-5, 5)
-    vy = randint(-5, 5)
+    a = randint(30, 70)
+    
+    vx = [-10, -9, -8, -7, 10, 9, 8, 7]
+    vy = [-10, -9, -8, -7, 10, 9, 8, 7]
     color = COLORS[randint(0, 5)]
     
-    Squares.append([x, y, vx, vy, a, color])
+    Squares.append([x+int(a/2), y+int(a/2), vx[randint(0, 7)], vy[(randint(0, 7))], a, color])
     
     
 
 
 def move_balls_and_squares():
     global Balls, score, Squares
-    screen.fill((226, 199, 199))
+    screen.fill((50, 199, 199))
     for number in range(len(Balls)):
         Ball = Balls[number]
         circle(screen, Ball[5], (Ball[0], Ball[1]), Ball[4])
+        #circle(screen, (0, 0, 0), (Ball[0], Ball[1]), Ball[4], 1)    #Выглядит стремненько
         if Ball[0] <= Ball[4] or Ball[0] >= 1200 - Ball[4]:
             Ball[2] = -Ball[2]
         if Ball[1] <= Ball[4] or Ball[1] >= 900 - Ball[4]:
@@ -64,17 +66,20 @@ def move_balls_and_squares():
     for number in range(len(Squares)):
         Square = Squares[number]
         x0 = Square[0] - int(Square[4]/2)
-        x0 = Square[0] - int(Square[4]/2)
-        rect(screen, Square[5], ())
-        if Ball[0] <= Ball[4] or Ball[0] >= 1200 - Ball[4]:
-            Ball[2] = -Ball[2]
-        if Ball[1] <= Ball[4] or Ball[1] >= 900 - Ball[4]:
-            Ball[3] = -Ball[3]
-        Ball[0] += Ball[2]
-        Ball[1] += Ball[3]
+        y0 = Square[1] - int(Square[4]/2)
+        rect(screen, Square[5], (x0, y0, Square[4], Square[4]))
+        #rect(screen, (0, 0, 0), (x0, y0, Square[4], Square[4]), 1)    #Выглядит стремненько
+        if Square[0] <= int(Square[4]/2) or Square[0] >= 1200 - int(Square[4]/2):
+            Square[2] = -Square[2]
+        if Square[1] <= int(Square[4]/2) or Square[1] >= 900 - int(Square[4]/2):
+            Square[3] = -Square[3]
+        Square[0] += Square[2]
+        Square[1] += Square[3]
         
+    pygame.display.update()     
+
         
-def click_check(event, number):
+def click_check_Ball(event, number):
     global score, Balls
     flag = True
     Ball = Balls[number]
@@ -84,7 +89,19 @@ def click_check(event, number):
         flag = not flag
     return flag
     
-    pygame.display.update()     
+    
+    
+def click_check_Square(event, number):
+    global score, Squares
+    flag = True
+    Square = Squares[number]
+    if abs(Square[0]-event[0]) <= int(Square[4]/2) and abs(Square[1]-event[1]) <= int(Square[4]/2):
+        score += 2
+    else:
+        flag = not flag
+    return flag
+    
+    
 
 pygame.display.update()
 clock = pygame.time.Clock()
@@ -92,30 +109,45 @@ finished = False
 
 
 Balls = []
+Squares = []
 score = 0 
 
-for i in range(10):
+
+print('Enter the number of balls')
+Balls_num = int(input())
+for i in range(Balls_num):
     new_ball()
-move_balls()
+
+print('Enter the number of squares')
+Squares_num = int(input())
+for i in range(Squares_num):
+    new_square()
+
+move_balls_and_squares()
 pygame.display.update()   
 while not finished:
     clock.tick(FPS)
-    move_balls()
+    move_balls_and_squares()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             finished = True
         elif event.type == pygame.MOUSEBUTTONDOWN:
             state = False
             for number in range(len(Balls)):
-                if click_check(event.pos, number):
+                if click_check_Ball(event.pos, number):
                     del(Balls[number])
                     new_ball()
+                    state = True
+            for number in range(len(Squares)):
+                if click_check_Square(event.pos, number):
+                    del(Squares[number])
+                    new_square()
                     state = True
             if state:
                 print('Sniper! Your score: ', score)
             else:
                 print('You have missed! Loooser ')
-            move_balls()
+            move_balls_and_squares()
             
             
             
